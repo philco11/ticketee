a9bac27b2e7d49c66672068fd6f57bab8ebba219
Philip Cortes <philip.cortes@gmail.com>
2013-03-06 16:03:49 -0800

    Add title functionality for show page, nice

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -19,5 +19,6 @@ class ProjectsController < ApplicationController
     def show 
       @project = Project.find(params[:id])
     end
+    
   end
 end
--- spec/integration/creating_projects_spec.rb
+++ spec/integration/creating_projects_spec.rb
@@ -4,9 +4,16 @@ feature 'Creating Projects' do
   scenario "can create a project" do
     visit '/'
     click_link 'New Project'
-    fill_in 'Name', :with => 'TextMate2'
+    fill_in 'Name', :with => 'TextMate 2'
     fill_in 'Description', :with => "A text-editor for OS X"
     click_button 'Create Project'
     page.should have_content('Project has been created.')
+    
+    project = Project.find_by_name("TextMate 2")
+    page.current_url.should == project_url(project)
+    title = "TextMate 2 - Projects - Ticketee"
+    find("title").should have_content(title)
+  
   end
+  
 end
