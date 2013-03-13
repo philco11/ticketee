226e4aad373be30cdc3160616beee5ccfad4d2ee
Philip Cortes <philip.cortes@gmail.com>
2013-03-13 14:11:28 -0700

    Lock down specific projects controller actions for admins only, pp 216, with three errrors...

--- spec/support/capybara_helpers.rb
+++ spec/support/capybara_helpers.rb
@@ -1,17 +1,18 @@
 module CapybaraHelpers
   
-  def assert_no_link_for(text)
+    def assert_no_link_for(text)
       page.should_not(have_css("a", :text => text),
       "Expected not to see the #{text.inspect} link, but did.")
-  end
+    end
   
-  def assert_link_for(text)
+    def assert_link_for(text)
     page.should(have_css("a", :text => text),
       "Expected to see the #{text.inspect} link, but did not.")
-  end
+    end
 
 end
 
 RSpec.configure do |config|
   config.include CapybaraHelpers, :type => :request
+  
 end
