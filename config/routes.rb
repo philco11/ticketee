776f3a821e9bc419ec75d1010c2060db1bfd998f
Philip Cortes <philip.cortes@gmail.com>
2013-03-19 13:10:46 -0700

    FIXED THE ADMIN_ROOT_PATH PROBLEMgit add . FUCK YEAHgit add .!  Still have 9 errors.

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,21 @@
 Ticketee::Application.routes.draw do
 
+get "/admin/users/index"
+
   devise_for :users
+
+
 
   root :to => "projects#index"
   resources :projects do
     resources :tickets
+  end
     
     namespace :admin do
       root :to => "base#index"
       resources :users
     end
-    
-    
-  end
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
